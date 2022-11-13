bb33639112b38f6569925d57b8f0f970e1c673ac
yszar <yszar@vip.qq.com>
2022-11-14 06:38:14 +0800

    feat: 接入微信公众平台
    增加接入微信公众平台验证功能

---
+++ main.py
@@ -0,0 +1,32 @@
+import hashlib
+
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv(verbose=True)
+
+WECHAT_TOKEN = os.getenv("WECHAT_TOKEN")
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/wechat/hello/")
+async def hello_wechat(signature: str, timestamp: str, nonce: str, echostr: str):
+    token = WECHAT_TOKEN
+    temp = [token, timestamp, nonce]
+    temp.sort()
+    res = hashlib.sha1("".join(temp).encode("utf8")).hexdigest()
+    return echostr if res == signature else {"errcode": 401, "errmsg": "access denied"}
