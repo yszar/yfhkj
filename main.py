8207decce12b41d7863c07aa8769124d8321b1d4
yszar <yszar@vip.qq.com>
2022-11-17 13:03:55 +0800

    feat(user): 新增黑名单功能

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi import Depends, FastAPI, Header, Response
 from dotenv import load_dotenv
 import os
 import datetime
+import time
 from pydantic import BaseModel
 from starlette.requests import Request
 
@@ -95,18 +96,49 @@ async def wechat_post(item: Item = Depends(XmlBody(Item)), header: str = Header(
     )
     cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
     msg_type = item["xml"]["MsgType"]
+    ts = datetime.datetime.now().timestamp()
     if msg_type == "text":
         content = item["xml"]["Content"]
         openid = item["xml"]["FromUserName"]
         match content:
             case content if content.isalnum() and len(content) == 6:
+                black_sql = f"SELECT * FROM `yfhkj_black_user` WHERE `openid`='{openid}'"
+                is_black = cursor.execute(black_sql)
+                if is_black >0:
+                    black = cursor.fetchone()
+                    un_sub_times = black["un_sub_times"]
+                    restore = black["restore"]
+                    if un_sub_times >= 2:
+                        return f"""<xml>
+                <ToUserName><![CDATA[{item["xml"]["FromUserName"]}]]></ToUserName>
+                <FromUserName><![CDATA[{item["xml"]["ToUserName"]}]]></FromUserName>
+                <CreateTime>{int(ts)}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[您无法使用此功能]]></Content>
+                </xml>"""
+                    elif restore == 1:
+                        resetore_sql = f"UPDATE `yfhkj_black_user` SET `restore`=0 WHERE `openid`={openid}"
+                        return f"""<xml>
+                <ToUserName><![CDATA[{item["xml"]["FromUserName"]}]]></ToUserName>
+                <FromUserName><![CDATA[{item["xml"]["ToUserName"]}]]></FromUserName>
+                <CreateTime>{int(ts)}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[系统检测到您有取消关注记录\n为防止误操作，您有一次恢复机会\n\n系统已为您自动恢复功能\n\n请注意！仅有这一次机会]]></Content>
+                </xml>"""
+                
                 sql = f"SELECT `times` FROM `yfhkj_queries` WHERE `openid`='{openid}' AND `date`='{datetime.date.today()}'"
                 cursor.execute(sql)
                 print("去数据库查密码")
                 sql = f"SELECT `password` FROM `yfhkj_my_pdf` WHERE `key`='{content}'"
                 zip_password = cursor.execute(sql)
                 if zip_password != 0:
-                    return cursor.fetchone()["password"]
+                    return f"""<xml>
+                <ToUserName><![CDATA[{item["xml"]["FromUserName"]}]]></ToUserName>
+                <FromUserName><![CDATA[{item["xml"]["ToUserName"]}]]></FromUserName>
+                <CreateTime>{int(ts)}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[解压密码：\n{cursor.fetchone()["password"]}\n\n切记不要取关！！！\n取关之后即便再次关注\n也无法再使用此功能]]></Content>
+                </xml>"""
                 else:
                     xml = f"""<xml>
                 <ToUserName><![CDATA[{item["xml"]["FromUserName"]}]]></ToUserName>
@@ -116,8 +148,10 @@ async def wechat_post(item: Item = Depends(XmlBody(Item)), header: str = Header(
                 <Content><![CDATA[输入错误，请输入正确的验证码]]></Content>
                 </xml>"""
                     return Response(content=xml, media_type="application/xml")
+            case content if content[0:2] == "n ":
+                return "success"
             case _:
-                pass
+                return "success"
     elif msg_type == "event":
         event = item["xml"]["Event"]
         match event:
@@ -128,14 +162,14 @@ async def wechat_post(item: Item = Depends(XmlBody(Item)), header: str = Header(
                 <FromUserName><![CDATA[{item["xml"]["ToUserName"]}]]></FromUserName>
                 <CreateTime>{int(ts)}</CreateTime>
                 <MsgType><![CDATA[text]]></MsgType>
-                <Content><![CDATA[感谢你关注——雨非黑科技\n\n需要什么PDF请直接回复名字\n\n原创整理:最新，最全，高清PDF]]></Content>
+                <Content><![CDATA[感谢你关注——雨非黑科技\n\n需要什么PDF请直接回复名字\n\n如果没有查询到您需要的资料\n请回复“n+空格+资料名称”\n例如：n C程序设计语言\n\n我会尽快为您搜集整理\n\n原创整理:最新，最全，高清PDF]]></Content>
                 </xml>"""
                 return Response(content=xml, media_type="application/xml")
             case "unsubscribe":
                 openid = item["xml"]["FromUserName"]
                 sql = (
                     f"INSERT INTO yfhkj_black_user(`openid`,`un_sub_times`,`restore`) VALUES ({openid},1,1) "
-                    f"ON DUPLICATE KEY UPDATE un_sub_times=un_sub_times+1,restore=restore-1;"
+                    f"ON DUPLICATE KEY UPDATE un_sub_times=un_sub_times+1;"
                 )
                 try:
                     cursor.execute(sql)
